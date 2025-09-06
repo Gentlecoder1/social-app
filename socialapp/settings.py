3a0a2425aa172905f094da900551e98606fac850
ISRAEL <israeloloruntoba3@gmail.com>
2025-09-06 15:34:39 +0100

    fix: Update ALLOWED_HOSTS to include the correct production domain

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -18,10 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["socialbook.onrender.com", "localhost", "127.0.0.1"]
-
-
-# Application definition
+ALLOWED_HOSTS = ["socialbook-puxe.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
